7e252781b65823f79d93bae03978efaa73157a83
C_Karadkar <c.karadkar@gmail.com>
2019-06-27 16:24:25 +0530

    Updated index.js with wdio 5 class

--- index.js
+++ index.js
@@ -1,41 +1,111 @@
-var util = require('util'), 
+let util = require('util'), 
   events = require('events'),
   path = require('path'),
   fs = require('fs'),
   mkdirp = require('mkdirp');
 
-var BrowserStackReporter = function(baseReporter, config, options = {}) {
+const WdioReporter = require('@wdio/reporter').default;
 
-  this.baseReporter = baseReporter
-  this.config = config
-  this.options = options
-  this.specs = {}
+class BrowserStackReporter extends WdioReporter {
 
-  this.on('end', function () {
-    onSingleFileOutput()
-  });
-
-  function onSingleFileOutput () {
-    const xml = prepareXml(baseReporter.stats.runners)
-    let filename = `REPORT-browserstack.all.xml` 
-    write(filename, xml)
+  constructor(options) {
+    options = Object.assign(options)
+    super(options)
   }
 
-  function prepareName(name = 'Skipped test') {
+  onRunnerStart(runner) {
+    console.log("====================onRunnerStart");
+  } 
+
+  onBeforeCommand(runner) {
+    console.log("====================onBeforeCommand");
+  } 
+
+  onAfterCommand(runner) {
+    console.log("====================onAfterCommand");
+  } 
+
+  onScreenshot(runner) {
+    console.log("====================onScreenshot");
+  } 
+
+  onSuiteStart(runner) {
+    console.log("====================onSuiteStart");
+  } 
+
+  onHookStart(runner) {
+    console.log("====================onHookStart");
+  } 
+
+  onHookEnd(runner) {
+    console.log("====================onHookEnd");
+  } 
+
+  onTestStart(runner) {
+    console.log("====================onTestStart");
+  } 
+
+  onTestPass(runner) {
+    console.log("====================onTestPass");
+  } 
+
+  onTestFail(runner) {
+    console.log("====================onTestFail");
+  } 
+
+  onTestSkip(runner) {
+    console.log("====================onTestSkip");
+  } 
+
+  onTestEnd(runner) {
+    console.log("====================onTestEnd");
+  } 
+
+  onSuiteEnd(runner) {
+    console.log("====================onSuiteEnd");
+  } 
+
+  onRunnerEnd(runner) {
+    console.log("====================onRunnerEnd");
+  } 
+
+  onComplete(runner){
+    console.log("==================== onComplete ++++++ +++ ");
+  }
+
+
+  // onAfterCommand(runner) {
+  //   // this.onSingleFileOutput();
+  //   console.log("====================onAfterCommand");
+  // } 
+
+  onSingleFileOutput () {
+    // console.log('==>> THIS dump :: ' + JSON.stringify(this));
+    // console.log('');
+    // console.log('====================== this.runnerStat :: ' + JSON.stringify(this.runnerStat));
+    const xml = this.prepareXml(this.runnerStat)
+    let filename = `REPORT-browserstack.all.xml`
+    this.write(filename, xml)
+  } // FUNC onSingleFileOutput END
+  
+  prepareName(name = 'Skipped test') {
     return name.split(/[^a-zA-Z0-9]+/).filter(
       (item) => item && item.length
     ).join('_')
-  }
+  } // FUNC prepareName END
 
-
-  function prepareXml(runners){
+  prepareXml(runners){
     var xmlbuilder = require('xmlbuilder');
     const builder = xmlbuilder.create('testsuites', {encoding: 'UTF-8', allowSurrogateChars: true})
     var testCaseIndex = 0;
+    // console.log('====================== NEW prepareXml runners :: ' + JSON.stringify(runners));
     for (const key of Object.keys(runners)) {
+      // console.log('=================>>> NEW key :: ' + key);
       const capabilities = runners[key]
-      const packageName = options.packageName
-          ? `${capabilities.sanitizedCapabilities}-${options.packageName}`
+      // console.log('=================>>> NEW prepareXml capabilities :: ' + JSON.stringify(capabilities));
+      // console.log('=================>>> NEW prepareXml options :: ' + JSON.stringify(this.options));
+      const packageName = this.options.packageName 
+          ? `${capabilities.sanitizedCapabilities}-${this.options.packageName}`
           : capabilities.sanitizedCapabilities
       for (let specId of Object.keys(capabilities.specs)) {
         const spec = capabilities.specs[specId]
@@ -46,13 +116,13 @@ var BrowserStackReporter = function(baseReporter, config, options = {}) {
           }
 
           const suite = spec.suites[suiteKey]
-          const suiteName = prepareName(suite.title)
+          const suiteName = this.prepareName(suite.title)
           const testSuite = builder.ele("testsuite", {name: suiteName})
 
           for (let testKey of Object.keys(suite.tests)) {
             if (testKey !== 'undefined') { 
               const test = suite.tests[testKey]
-              const testName = prepareName(test.title)
+              const testName = this.prepareName(test.title)
               const testCase = testSuite.ele("testcase",{name: testName, id: `${suiteName}.${testName}{0}`, index: 0 });
               testCase.ele("session", {}, runners[key].sessionID);
               if (test.state === 'pending') {
@@ -64,9 +134,9 @@ var BrowserStackReporter = function(baseReporter, config, options = {}) {
       }
     }
     return builder.end({ pretty: true});
-  }
+  } // FUNC prepareXml END
 
-  function write(filename, xml) {
+  write(filename, xml) {
 
     var outputDir = "."
     if (this.options && typeof this.options.outputDir == 'string') {
@@ -84,13 +154,8 @@ var BrowserStackReporter = function(baseReporter, config, options = {}) {
       console.log(`Failed to write xunit report "${filename}"
        to [${outputDir}]. Error: ${e}`)
     }
-  }
+  } // FUNC write END
 
-  function format (val) {
-    return JSON.stringify(baseReporter.limit(val))
-  }
 }
 
-util.inherits(BrowserStackReporter, events.EventEmitter);
-
-module.exports = BrowserStackReporter
+module.exports.default = BrowserStackReporter
